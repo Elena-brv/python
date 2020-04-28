8711922c6af69d45dfb569b1f2a98e3a4c37e4ac
Elena-brv <64437769+Elena-brv@users.noreply.github.com>
2020-04-28 10:18:45 +0300

    python lesson1

---
+++ task2.py
@@ -0,0 +1,5 @@
+import math
+
+r = float(input('radius = '))
+area = r**2*math.pi
+print('площадь круга = ',area)
---
+++ task4.py
@@ -0,0 +1,7 @@
+a = "Как"
+b = "ваши"
+c = "дела?"
+
+print (a, end =' ')
+print (b, end =' ')
+print (c, end =' ')
