11cf80cfb9df96209f495f078894f1793f33026e
Elena-brv <64437769+Elena-brv@users.noreply.github.com>
2020-05-09 11:29:59 +0300

    Update task 3.3.py

--- lesson 3 tasks/task 3.3.py
+++ lesson 3 tasks/task 3.3.py
@@ -1,5 +1,5 @@
 def compare(a, b):
-    if a == b or a + b == 5 or max(a, b) - min(a, b) == 5:
+    if a == b or a + b == 5 or abs(a - b) == 5:
         print('True')
     else:
         print('False')
