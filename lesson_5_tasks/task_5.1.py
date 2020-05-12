24ed26839efc7623873f8a4c0f2ff8bbf1c4df96
Elena-brv <64437769+Elena-brv@users.noreply.github.com>
2020-05-12 17:05:50 +0300

    Update task_5.1.py

--- lesson_5_tasks/task_5.1.py
+++ lesson_5_tasks/task_5.1.py
@@ -1,9 +1,5 @@
-# with open('script_copy.py', 'w+') as output, open('task_5.1.py', 'r') as input: output.write(input.read())
-
 file = open('script_copy.py', 'w')
-
 file2 = open('task_5.1.py', 'r')
-
 file.write(file2.read())
 
 file.close()
