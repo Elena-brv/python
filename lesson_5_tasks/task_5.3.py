9f495a097250d19bde444c2cb58fb95829fdcc3e
Elena-brv <64437769+Elena-brv@users.noreply.github.com>
2020-05-15 12:17:08 +0300

    Update task_5.3.py

--- lesson_5_tasks/task_5.3.py
+++ lesson_5_tasks/task_5.3.py
@@ -1,9 +1,9 @@
 import time, random
 
-def elements(i):
+def elements():
     for i in range(100000):
         print(random.randint(0,1000))
 
 start_time = time.time()
-elements(0)
+elements()
 print(time.strftime('%S'),'\n',(time.time() - start_time),'sec')
