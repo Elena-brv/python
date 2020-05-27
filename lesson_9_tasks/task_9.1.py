e45825eaa54f635bc46f322d3e21f63965e07752
Elena-brv <64437769+Elena-brv@users.noreply.github.com>
2020-05-27 12:56:32 +0300

    Update task_9.1.py

--- lesson_9_tasks/task_9.1.py
+++ lesson_9_tasks/task_9.1.py
@@ -5,18 +5,18 @@ class Rectangle:
         self.side2 = side2
     
     def square(self):
-        print(f'Square of rectangle = {self.side1 * self.side2}') 
-    
+        return self.side1 * self.side2
+
     def perimeter(self):
-        print(f'Perimeter of rectangle = {2 * (self.side1 + self.side2)}')
+        return 2 * (self.side1 + self.side2)
 
 
 rectangle = Rectangle(10, 20)
 
 print(f'side1 = {rectangle.side1}, side2 = {rectangle.side2}')
 
-rectangle.square()
-rectangle.perimeter()
-
+square = rectangle.square()
+perimeter = rectangle.perimeter()
+print(f'Square of rectangle = {square}, perimeter = {perimeter}')
 
 
